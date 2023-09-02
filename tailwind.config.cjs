d08996b79cda2e851c41c0419dbaae2f77ce58c2
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-09-02 13:16:53 -0300

    add resources and basic styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(233, 26%, 24%)',
+        'lime-green': 'hsl(136, 65%, 51%)',
+        'bright-cyan': 'hsl(192, 70%, 51%)',
+        'grayish-blue': 'hsl(233, 8%, 62%)',
+        'light-grayish-blue': 'hsl(220, 16%, 96%)',
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+      },
+      fontFamily: {
+        'public-sans': ['Public Sans','Inter', 'sans-serif'],
+      },
+    },
+
+  },
+  plugins: [],
 }
